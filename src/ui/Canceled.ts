ccfef783be4cc45b89498b220cb5795e609708d0
CibNumeritos <imcib2498.212244.2808@gmail.com>
2024-09-12 22:12:07 -0500

    feat: Canceled decorator now includes cancelation reason

--- src/ui/Canceled.ts
+++ src/ui/Canceled.ts
@@ -1,9 +1,10 @@
 import { Player } from "@minecraft/server";
+import { FormCancelationReason } from "@minecraft/server-ui";
 
 function Canceled(
     target: any,
     propertyKey: string,
-    descriptor: TypedPropertyDescriptor<(player: Player) => void>,
+    descriptor: TypedPropertyDescriptor<(player: Player, reason: FormCancelationReason) => void>,
 ) {
     target.constructor["_canceled"] = descriptor.value;
 }
